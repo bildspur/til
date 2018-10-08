1dff8dc48b2fa03a006d1716017c7bb5e277573b
Florian Bruggisser <florian@bildspur.ch>
2018-10-08 15:53:36 +0200

    added blob detection to movement detecton script

--- openmv/MovementDetection.py
+++ openmv/MovementDetection.py
@@ -3,8 +3,6 @@
 
 import sensor, image, pyb, os, time
 
-TRIGGER_THRESHOLD = 5
-
 sensor.reset()                      # Reset and initialize the sensor.
 sensor.set_pixformat(sensor.GRAYSCALE) # Set pixel format to RGB565 (or GRAYSCALE)
 sensor.set_framesize(sensor.QVGA)   # Set frame size to QVGA (320x240)
@@ -12,6 +10,16 @@ sensor.skip_frames(time = 2000)     # Wait for settings take effect.
 sensor.set_auto_whitebal(True)
 clock = time.clock()                # Create a clock object to track the FPS.
 
+# variables
+trigger_threshold = 5
+
+# blob detection
+blob_threshold = (5, 255)
+pixels_threshold = 250
+area_threshold = 250
+
+
+last_frame_triggered = False
 extra_fb = sensor.alloc_extra_fb(sensor.width(), sensor.height(), sensor.GRAYSCALE)
 
 sensor.skip_frames(time = 2000) # Give the user time to get ready.
@@ -30,19 +38,24 @@ while(True):
     # value. The difference between the two values will grow as the difference
     # image seems more pixels change.
     diff = hist.get_percentile(0.99).l_value() - hist.get_percentile(0.90).l_value()
-    triggered = diff > TRIGGER_THRESHOLD
-
-    print(clock.fps(), triggered) # Note: Your OpenMV Cam runs about half as fast while
-    # connected to your computer. The FPS should increase once disconnected.
+    triggered = diff > trigger_threshold
 
     # send framebuffer to ide
     # print(extra_fb.compressed_for_ide(), end="")
 
-    # find direction
+    blobs = extra_fb.find_blobs([blob_threshold], pixels_threshold=pixels_threshold, area_threshold=area_threshold, merge=True)
+    extra_fb.replace(img)
+
+    # find direction by blob detection
     if(triggered):
-        # detect blob an calculate new position
-        print("triggered")
+        for blob in blobs:
+            img.draw_rectangle(blob.rect())
+            img.draw_cross(blob.cx(), blob.cy())
+
+            # send trigger with position
+            print("%s: %s | %s" % (clock.fps(), blob.cx(), blob.cy()))
 
 
     # update framebuffer
-    extra_fb.replace(img)
+    #extra_fb.replace(img)
+    last_frame_triggered = triggered
