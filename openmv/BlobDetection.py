ea486bf7d07f1f6c5e62d74661f364735fc7da3f
Florian Bruggisser <florian@bildspur.ch>
2018-10-08 15:53:22 +0200

    added blob detection as special script

---
+++ openmv/BlobDetection.py
@@ -0,0 +1,23 @@
+# Untitled - By: cansik - Mon Oct 8 2018
+
+import sensor, image, time
+
+sensor.reset()
+sensor.set_pixformat(sensor.GRAYSCALE)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time = 500)
+
+clock = time.clock()
+
+blob_threshold = (230, 255)
+
+while(True):
+    clock.tick()
+    img = sensor.snapshot()
+    blobs = img.find_blobs([blob_threshold], pixels_threshold=100, area_threshold=100, merge=True)
+
+    for blob in blobs:
+        img.draw_rectangle(blob.rect())
+        img.draw_cross(blob.cx(), blob.cy())
+
+    print(clock.fps())
